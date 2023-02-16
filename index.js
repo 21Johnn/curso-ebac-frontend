d9de0fcf15148bf4eb3abce8e564356e7e463da6
John <mktkamehouse@gmail.com>
2023-02-15 23:57:48 -0300

    exercicio ts

---
+++ index.js
@@ -0,0 +1,8 @@
+function multiply(a, b) {
+    return a * b;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiply(10, 10));
+console.log(saudacao("John"));
---
+++ index.ts
@@ -0,0 +1,10 @@
+function multiply( a : number , b : number){
+    return a * b
+}
+
+function saudacao(nome: string){
+    return "Olá " + nome;
+}
+
+console.log(multiply(10, 10));
+console.log(saudacao("John"))
